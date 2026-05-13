75a0533b02b49e977d11e8fd4570dff6f4813fb1
user241324 <tom.rob.one@gmail.com>
2026-05-13 08:57:36 -0600

    Fix VRR value and correct environment variable syntax

--- HOME/.config/hypr/hyprland.lua
+++ HOME/.config/hypr/hyprland.lua
@@ -20,7 +20,7 @@ hl.monitor({
     mode     = "2560x1440@180",
     position = "auto",
     scale    = "1",
-    vrr      = "2",
+    vrr      = 2,
     --HDR Settings
     bitdepth = 10,
     cm = "hdr",
@@ -71,14 +71,14 @@ hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("XCURSOR_THEME", "breeze_cursors")
 
 -- GTK
-hl.env("GDK_BACKEND", "wayland", "x11", *"
-hl.env("GTK_THEME", "Breeze-Dark"
+hl.env("GDK_BACKEND", "wayland", "x11", "*")
+hl.env("GTK_THEME", "Breeze-Dark")
 
 --QT
-hl.env("QT_QPA_PLATFORM", "wayland;xcb"
-hl.env("QT_QPA_PLATFORMTHEME", "qt6ct"
-hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1"
-hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1"
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 
 -----------------------
 ----- PERMISSIONS -----
