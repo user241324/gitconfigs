e4bdb6cc45746544249d6f974c79f1d132c29b45
user241324 <tom.rob.one@gmail.com>
2026-05-13 11:50:57 -0600

    Remove HDR settings from monitor config
    Removed HDR settings from the monitor configuration.

--- HOME/.config/hypr/monitor/2560x1440@180.lua
+++ HOME/.config/hypr/monitor/2560x1440@180.lua
@@ -4,14 +4,4 @@ hl.monitor({
     position = "auto",
     scale = "1",
     vrr = 2,
-    --HDR Settings
-    bitdepth = 10,
-    cm = "hdr",
-    sdrbrightness = 1.2,
-    sdrsaturation = 1,
-    sdr_min_luminance = 0.005,
-    sdr_max_luminance = 300,
-    min_luminance = 0.005,
-    max_luminance = 1000,
-    max_avg_luminance = 300,
 })
